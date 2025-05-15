9355bc0a615253daae0509c4df8ba3542cd6fb6d
hiroki-96 <nomoclo89@gmail.com>
2025-05-15 22:44:56 +0900

    itemsテーブルを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_14_033701) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_15_134347) do
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
