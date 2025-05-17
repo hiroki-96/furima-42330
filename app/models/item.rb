3d48d660d1ceef35165851b0b671b90d8e78cdf0
hiroki-96 <nomoclo89@gmail.com>
2025-05-17 19:35:16 +0900

    プルダウン表示の設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,26 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :delivery_time
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :price
+    validates :image
+  end
+
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :delivery_time_id
+  end
 end
