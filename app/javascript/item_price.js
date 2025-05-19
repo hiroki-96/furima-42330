4e1670eb92c48f42881c9dcfee67c6714673658b
hiroki-96 <nomoclo89@gmail.com>
2025-05-19 16:49:15 +0900

    renderメソッドに対応したイベントを追加

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,22 +1,25 @@
-document.addEventListener('turbo:load', () => {
-  const priceInput = document.getElementById('item-price');
+const price = () => {
+  const priceInput = document.getElementById("item-price");
 
-  if (priceInput) {
-    priceInput.addEventListener('input', () => {
-      const inputValue = parseInt(priceInput.value);
+  if (!priceInput) return;
 
-      const taxDom = document.getElementById('add-tax-price');
-      const profitDom = document.getElementById('profit');
+  priceInput.addEventListener("input", () => {
+    const inputValue = parseInt(priceInput.value);
 
-      if (inputValue >= 300 && inputValue <= 9999999) {
-        const tax = Math.floor(inputValue * 0.1);
-        const profit = inputValue - tax;
-        taxDom.innerHTML = tax.toLocaleString();
-        profitDom.innerHTML = profit.toLocaleString();
-      } else {
-        taxDom.innerHTML = '';
-        profitDom.innerHTML = '';
-      }
-    });
-  }
-});
+    const taxDom = document.getElementById("add-tax-price");
+    const profitDom = document.getElementById("profit");
+
+    if (inputValue >= 300 && inputValue <= 9999999) {
+      const tax = Math.floor(inputValue * 0.1);
+      const profit = inputValue - tax;
+      taxDom.innerHTML = tax.toLocaleString();
+      profitDom.innerHTML = profit.toLocaleString();
+    } else {
+      taxDom.innerHTML = '';
+      profitDom.innerHTML = '';
+    }
+  });
+};
+
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
