4ade2e6958cacc50a5c84da3c95d371bfd4e406e
hiroki-96 <nomoclo89@gmail.com>
2025-05-23 18:25:34 +0900

    ordersコントローラーのnewアクション内を編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,8 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
   def new
+    @item = Item.find(params[:item_id])
+    @order_form = OrderForm.new
   end
 
   def create
