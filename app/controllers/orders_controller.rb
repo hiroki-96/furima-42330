635f6110cef4d25c0c8a8a334ea65deb3205635c
hiroki-96 <nomoclo89@gmail.com>
2025-05-24 15:41:36 +0900

    Fromオブジェクトの作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,9 +4,17 @@ class OrdersController < ApplicationController
   before_action :redirect_if_seller
 
   def new
+    @order_address = OrderAddress.new
   end
 
   def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 end
 
@@ -14,6 +22,21 @@ private
 
 def set_item
   @item = Item.find(params[:item_id])
+end
+
+def order_params
+  params.require(:order_address).permit(
+    :postal_code,
+    :prefecture_id,
+    :city,
+    :block,
+    :building,
+    :phone_number,
+    :token
+  ).merge(
+    user_id: current_user.id,
+    item_id: params[:item_id]
+  )
 end
 
 def redirect_if_seller
