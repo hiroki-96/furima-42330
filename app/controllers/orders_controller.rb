172995c82eff0149bfbaa1b19d6bb7e14f3acaa8
hiroki-96 <nomoclo89@gmail.com>
2025-05-24 11:27:56 +0900

    自身が出品した商品の商品購入ページに遷移しようとすると、トップページに遷移

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,24 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_item
+  before_action :redirect_if_seller
+
   def new
     @item = Item.find(params[:item_id])
   end
 
   def create
   end
+end
+
+private
+
+def set_item
+  @item = Item.find(params[:item_id])
+end
+
+def redirect_if_seller
+  return unless current_user.id == @item.user_id
+
+  redirect_to root_path
 end
