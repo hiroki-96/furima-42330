1c35ed4799df909b22e383e51c9d8567aee56624
hiroki-96 <nomoclo89@gmail.com>
2025-05-27 17:03:49 +0900

    テストコードの実装

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :order_address do
     postal_code    { '123-4567' }
     prefecture_id  { 2 }
-    city           { '横浜市緑区' }
+    city           { '横浜市' }
     block          { '青山1-1-1' }
     building       { '柳ビル103' }
     phone_number   { '09012345678' }
