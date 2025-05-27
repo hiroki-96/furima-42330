2a38dbd6249d11f926e25ae00f643904b69676f2
hiroki-96 <nomoclo89@gmail.com>
2025-05-27 12:07:42 +0900

    クレジットカード購入機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,15 +4,18 @@ class OrdersController < ApplicationController
   before_action :redirect_if_seller
 
   def new
+    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
     @order_address = OrderAddress.new
   end
 
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
+      gon.public_key = ENV['PAYJP_PUBLIC_KEY']
       render :new, status: :unprocessable_entity
     end
   end
@@ -35,7 +38,18 @@ def order_params
     :token
   ).merge(
     user_id: current_user.id,
-    item_id: params[:item_id]
+    item_id: params[:item_id],
+    token: params[:token]
+  )
+end
+
+def pay_item
+  Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+  item = Item.find(order_params[:item_id])
+  Payjp::Charge.create(
+    amount: item.price, # 商品の値段
+    card: order_params[:token], # カードトークン
+    currency: 'jpy' # 通貨の種類（日本円）
   )
 end
 
